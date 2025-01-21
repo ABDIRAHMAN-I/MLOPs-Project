a1fd1519d19bd32949b6b20fd4dd89cc96be4c2b
ABDIRAHMAN-I <abdirahman.i@hotmail.com>
2025-01-21 12:59:45 +0000

    create locals values

---
+++ terraform/locals.tf
@@ -0,0 +1,12 @@
+locals {
+  name   = "mlops-project"
+  domain = "lab.abdirahman-ismail.com"
+  region = "eu-west-2"
+
+  tags = {
+
+    project = "MLOPs-Advanced-project"
+    owner   = "Abdirahman"
+  }
+
+}
